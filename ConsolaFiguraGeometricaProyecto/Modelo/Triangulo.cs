28fb520f2913349e04b13ca83213352930ef7db1
Enrique Alonso <0198748@up.edu.mx>
2019-08-20 20:39:00 -0500

    Addition of Triangle

---
+++ ConsolaFiguraGeometricaProyecto/Modelo/Triangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsolaFiguraGeometricaProyecto.Interface;
+namespace ConsolaFiguraGeometricaProyecto.Modelo
+{
+    class Triangulo:IFiguraGeometrica
+    {
+        private float _base, _altura;
+        private string name="Triangulo";
+
+        public float CalcularArea()
+        {
+            return _base*_altura/2;
+        }
+        public float CalcularPerimetro()
+        {
+            return _base*3;
+        }
+        public string ObtenerNombre()
+        {
+            return name;
+        }
+
+        public Triangulo(float b, float a)
+        {
+            _base = b;
+            _altura = a;
+        }
+    }
+}
--- ConsolaFiguraGeometricaProyecto/Program.cs
+++ ConsolaFiguraGeometricaProyecto/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsolaFiguraGeometricaProyecto.Interface;
+using ConsolaFiguraGeometricaProyecto.Modelo;
 
 namespace ConsolaFiguraGeometricaProyecto
 {
@@ -11,6 +12,9 @@ namespace ConsolaFiguraGeometricaProyecto
     {
         static void Main(string[] args)
         {
+            var triangulo = new Triangulo(3, 2);
+            mostrarInformacion(triangulo);
+            Console.ReadKey();
         }
 
         public static void mostrarInformacion(IFiguraGeometrica figura)
